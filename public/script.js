5f9fc52e2d81ad7f03ab955a49a8545d8c227470
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-02-13 09:50:47 +0100

    Javascript toegevoegd aan mijn button

---
+++ public/script.js
@@ -0,0 +1,10 @@
+// opent en sluit de button
+let filterBtn = document.querySelector('button')
+
+filterBtn.addEventListener('click', function(){
+    document.querySelector('.sorteren').classList.toggle('visible')
+})
+// filtert de button
+
+// link naar profilecard
+let card = document.querySelector('#data-profile-card')
